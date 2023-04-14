910237199ed22462dddd2cb84bb4e6c12095112e
Job <jobtalle@hotmail.com>
2023-04-14 12:27:05 +0200

    Canvas is cleared

--- js/smoke.js
+++ js/smoke.js
@@ -1,9 +1,18 @@
+import {Color} from "./color.js";
+import {gl} from "./gl.js";
+
 export class Smoke {
+    static #CLEAR_COLOR = new Color("#74a7b6");
+
+    constructor() {
+        gl.clearColor(Smoke.#CLEAR_COLOR.r, Smoke.#CLEAR_COLOR.g, Smoke.#CLEAR_COLOR.b, 1);
+    }
+
     update() {
 
     }
 
     render(time) {
-
+        gl.clear(gl.COLOR_BUFFER_BIT);
     }
 }
